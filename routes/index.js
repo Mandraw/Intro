d903a9be325735ea1a8f7321aa10c71b8ab3b002
Oscar <M_Mandrake@hotmail.fr>
2018-03-07 11:09:05 +0100

    contact add

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+try {
+  var dataJSON = require('../model/data');
+}
+catch(error) {
+  console.error("JSON Fail" + error);
+}
+
+router.get('/:page', function(req, res, next) {
+  res.render('index', {page : req.params.page , json : dataJSON });
+})
+.get('/', function(req, res, next) {
+  res.render('index', {json : dataJSON });
 });
 
 module.exports = router;
